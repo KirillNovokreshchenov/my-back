ed243550019b6c0c3bd35e8b465a395e736897fd
KirillNovokreshchenov <kirillnovokrest@gmail.com>
2023-05-04 05:58:34 +0300

    change validation

--- src/middlewares/blog-middlewares.ts
+++ src/middlewares/blog-middlewares.ts
@@ -1,8 +1,8 @@
 import {body} from "express-validator";
 
 
-export const nameValidation = body('name').isString().isLength({max: 15}).withMessage('incorrect name, max length 15 symbols')
-export const descriptionValidation = body('description').isString().isLength({max: 500}).withMessage('incorrect description, max length 500 symbols')
-export const websiteUrlValidation = body('websiteUrl').isString().isLength({max:100}).matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/).withMessage('incorrect website URL')
+export const nameValidation = body('name').isString().trim().isLength({min: 1, max: 15}).withMessage('incorrect name, max length 15 symbols')
+export const descriptionValidation = body('description').isString().trim().isLength({min: 1, max: 500}).withMessage('incorrect description, max length 500 symbols')
+export const websiteUrlValidation = body('websiteUrl').isString().trim().isLength({min: 1, max:100}).matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/).withMessage('incorrect website URL')
 
 
