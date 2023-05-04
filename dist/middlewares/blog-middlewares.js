2d9d7ca4c6b2692b8312c7ae33da5dc92d6e0da6
KirillNovokreshchenov <kirillnovokrest@gmail.com>
2023-05-04 05:58:46 +0300

    change validation

--- dist/middlewares/blog-middlewares.js
+++ dist/middlewares/blog-middlewares.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.websiteUrlValidation = exports.descriptionValidation = exports.nameValidation = void 0;
 const express_validator_1 = require("express-validator");
-exports.nameValidation = (0, express_validator_1.body)('name').isString().isLength({ max: 15 }).withMessage('incorrect name, max length 15 symbols');
-exports.descriptionValidation = (0, express_validator_1.body)('description').isString().isLength({ max: 500 }).withMessage('incorrect description, max length 500 symbols');
-exports.websiteUrlValidation = (0, express_validator_1.body)('websiteUrl').isString().isLength({ max: 100 }).matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/).withMessage('incorrect website URL');
+exports.nameValidation = (0, express_validator_1.body)('name').isString().trim().isLength({ min: 1, max: 15 }).withMessage('incorrect name, max length 15 symbols');
+exports.descriptionValidation = (0, express_validator_1.body)('description').isString().trim().isLength({ min: 1, max: 500 }).withMessage('incorrect description, max length 500 symbols');
+exports.websiteUrlValidation = (0, express_validator_1.body)('websiteUrl').isString().trim().isLength({ min: 1, max: 100 }).matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/).withMessage('incorrect website URL');
