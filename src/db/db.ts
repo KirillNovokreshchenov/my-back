4af6dce695dc4b7e84ed50c6472617969e115710
KirillNovokreshchenov <kirillnovokrest@gmail.com>
2023-05-14 04:37:59 +0300

    fix

--- src/db/db.ts
+++ src/db/db.ts
@@ -9,15 +9,14 @@ const uri = process.env.MONGO_URI
 
 const client = new MongoClient(uri);
 
-const db = client.db('project')
+const db = client.db('projectBlogsAndPosts')
 export const collectionBlogs = db.collection<BlogType>('blogs')
 export const collectionPosts = db.collection<PostType>('posts')
-
 
 export async function runDB() {
     try {
         await client.connect();
-        await client.db("project").command({ ping: 1 });
+        await client.db("projectBlogsAndPosts").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         //await client.close();
