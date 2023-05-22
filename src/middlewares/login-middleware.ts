b2fd8be718d702d019f47d88b8a5b8defcf9c36a
KirillNovokreshchenov <kirillnovokrest@gmail.com>
2023-05-22 22:59:42 +0300

    add users fix

--- src/middlewares/login-middleware.ts
+++ src/middlewares/login-middleware.ts
@@ -2,12 +2,8 @@ import {collectionUsers} from "../db/db";
 import {errorsValidationMiddleware} from "./err-middleware";
 import {body} from "express-validator";
 
-const loginOrEmailValidation = body('loginOrEmail').custom(async (value)=>{
-    const existUser = await collectionUsers.findOne({$or: [{login: value}, {email: value}]})
-    if(!existUser){
-        throw new Error('incorrect loginOrEmail')
-    }
-})
+const loginOrEmailValidation = body('loginOrEmail').isString().trim().notEmpty()
+const passwordValidation = body('password').isString().trim().notEmpty()
 
 
-export const loginValidation = [loginOrEmailValidation, errorsValidationMiddleware]
+export const loginValidation = [loginOrEmailValidation, passwordValidation, errorsValidationMiddleware]
